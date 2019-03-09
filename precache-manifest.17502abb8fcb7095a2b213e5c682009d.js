bf4345d9d0e02003a04e2db3f6a8767c0cd780a6
Sandro Tonon <tonon.san@gmail.com>
2019-03-09 12:17:18 +0100

    Updates

---
+++ precache-manifest.17502abb8fcb7095a2b213e5c682009d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd6001b78e76f8a3c01171aef6ab7a29",
+    "url": "/glur4k/got-calc/static/media/GameofThrones.cd6001b7.woff2"
+  },
+  {
+    "revision": "c195413625cb5dded5cbb2605dec4395",
+    "url": "/glur4k/got-calc/static/media/GameofThrones.c1954136.woff"
+  },
+  {
+    "revision": "508611ebdd0497db9b3a3e81175bc1e1",
+    "url": "/glur4k/got-calc/static/media/GameofThrones.508611eb.ttf"
+  },
+  {
+    "revision": "802a9e25e325c15bd94c",
+    "url": "/glur4k/got-calc/static/js/runtime~main.b64744fa.js"
+  },
+  {
+    "revision": "68e0bffbd9c67c1ffe08",
+    "url": "/glur4k/got-calc/static/js/main.1745d623.chunk.js"
+  },
+  {
+    "revision": "4041f1d75317cf974978",
+    "url": "/glur4k/got-calc/static/js/2.72c461a0.chunk.js"
+  },
+  {
+    "revision": "68e0bffbd9c67c1ffe08",
+    "url": "/glur4k/got-calc/static/css/main.a68c7427.chunk.css"
+  },
+  {
+    "revision": "96ff218a002dafc75a9e7836fdd53b82",
+    "url": "/glur4k/got-calc/index.html"
+  }
+];
